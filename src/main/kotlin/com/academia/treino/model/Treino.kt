0decb9b21a08873199d2ed1d2a1826ed7b28e55c
Jefferson <jeffersoneuclides84@gmail.com>
2021-04-10 21:56:01 -0300

    Solving id problem

--- src/main/kotlin/com/academia/treino/model/Treino.kt
+++ src/main/kotlin/com/academia/treino/model/Treino.kt
@@ -4,4 +4,4 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-class Treino (var titulo: String, var descricao: String, var videoUrl: String, @Id var _id: Long? = null)
+class Treino (var titulo: String, var descricao: String, var videoUrl: String, @Id var _id: String? = null)
